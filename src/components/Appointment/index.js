4b939d8975b6b5f6ce3bd876e435b64f20f867ee
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-10 18:48:24 -0400

    conditionally rendering form elements now

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,44 @@
 import React from "react";
 
+import { useVisualMode } from "hooks/useVisualMode"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import ("components/Appointment/styles.scss")
+import Form from "components/Appointment/Form";
+import("components/Appointment/styles.scss")
 
-export default function Appointment(props){
-  return(
+// console.log(useVisualMode)
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  return (
+    // <article className="appointment">
+    //   <Header time = {props.time}/>
+    //   {props.interview ?  
+    //   <Show 
+    //     student = {props.interview.student}
+    //     interviewer = {props.interview.interviewer}
+    //   /> : <Empty />}
+    // </article>
     <article className="appointment">
-      <Header time = {props.time}/>
-      {props.interview ? 
-      <Show 
-        student = {props.interview.student}
-        interviewer = {props.interview.interviewer}
-      /> : <Empty />}
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers = {[]}
+        />
+      )}
     </article>
   )
 }
