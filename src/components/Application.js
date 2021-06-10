77a85a82878b994bc63b89e87e23671a4ce30543
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-10 13:20:29 -0400

    importing helper function and using it to set dailyAppointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,51 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 
+import {getAppointmentsForDay} from '../helpers/selectors'
 
 import DayList from './DayList';
 import Appointment from "components/Appointment/"
 import InterviewerListItem from './InterviewerListItem'
 import "components/Application.scss";
-
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 6,
-//     time: "3pm",
-//     interview: {
-//       student: "Tawfiq Khan",
-//       interviewer: {
-//         id: 1,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "4pm",
-//   },
-// ];
 
 export default function Application(props) {
 
@@ -55,22 +16,23 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days : [],
-    appointments: {}
+    appointments: {},
+    interviewers : []
   })
-
-  const dailyAppointments = [];
+  console.log(getAppointmentsForDay)
+  const dailyAppointments = getAppointmentsForDay(state, "Monday")
 
   const setDay = day => setState(prevState => ({...prevState, day}))
   // const setDays = days => setState(prevState => ({...prevState, days}))
   // setState(prev => ({ ...prev, days }));
   
-  const interviewers = [
-    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  ];
+  // const interviewers = [
+  //   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  //   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  //   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  //   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  //   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  // ];
   const allAppointments = dailyAppointments.map(appointment => {
     return (
       <Appointment
@@ -86,12 +48,13 @@ export default function Application(props) {
     //   })
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers'),
     ]).then((all)=> {
       const daysData = all[0].data;
       const appointmentsData = all[1].data
-      setState(prevState => ({...prevState, days: daysData, appointments: appointmentsData}))
-      // dailyAppointments = state.appointments;
+      const interviewersData = all[1].data
+      setState(prevState => ({...prevState, days: daysData, appointments: appointmentsData, interviewers : interviewersData}))
     })
   }, [])
   return (
