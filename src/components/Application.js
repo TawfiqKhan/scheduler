56f753aa9776e44e88bdf9a9f5c63ffb88d36f78
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-10 13:38:42 -0400

    calling getappointmentsForDay function using day from state instead of hardcoded day

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,25 +14,22 @@ export default function Application(props) {
   // const [days, setDays] = useState([])
 
   const [state, setState] = useState({
-    day: "Monday",
+    day: "",
     days : [],
     appointments: {},
     interviewers : []
   })
   console.log(getAppointmentsForDay)
-  const dailyAppointments = getAppointmentsForDay(state, "Monday")
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
 
-  const setDay = day => setState(prevState => ({...prevState, day}))
+  // const setDay = day => setState(prevState => ({...prevState, day: day}))
+  const setDay = (day)=> {
+    console.log(day)
+    setState(prevState => ({...prevState, day}))
+  }
   // const setDays = days => setState(prevState => ({...prevState, days}))
   // setState(prev => ({ ...prev, days }));
-  
-  // const interviewers = [
-  //   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  //   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  //   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  //   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  //   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-  // ];
+
   const allAppointments = dailyAppointments.map(appointment => {
     return (
       <Appointment
