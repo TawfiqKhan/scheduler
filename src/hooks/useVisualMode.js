be25e80d226b73a13d0b659de7dcb920586b176b
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-10 17:40:15 -0400

    implementing back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,21 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
- const useVisualMode=  (param) =>{
-  const [mode, setMode] = useState(param)
+const useVisualMode = (initialMode) => {
+  // const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
-   function transition(newMode){
-    return setMode(newMode)
-   }
-  return {mode, transition}
+  function transition(newMode) {
+    setHistory(prev => [...prev, newMode])
+    // return setMode(newMode)
+  }
+  function back(){
+    let lastItem = history[history.length - 1]
+    if(history.length === 1) {
+      return history;
+    }
+    setHistory(prevHistory => prevHistory.filter(item => item !==lastItem))
+  }
+  return { mode: history[history.length - 1], transition, back }
 }
 
-export {useVisualMode}
+export { useVisualMode }
