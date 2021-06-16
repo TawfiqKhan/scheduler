a5e11e446165428ed60c47da66f3696d573c5faa
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-15 21:34:04 -0400

    adding initial test for Application

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
+describe("defaults to Monday and changes the schedule when a new day is selected.", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const RenderedComponent = render(<Application />);
+
+    await RenderedComponent.findByText("Monday").then(() => {
+      const button = RenderedComponent.getByText('Tuesday')
+      fireEvent.click(button)
+      expect(RenderedComponent.getByText("Leopold Silvers")).toBeInTheDocument();
+    })
+  });
 });
--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -44,34 +44,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  // it("calls onSave function when the name is defined", () => {
-  //   const onSave = jest.fn();
-  //   const renderedComponent = render(
-  //     <Form interviewers={interviewers} onSave={onSave} student='Lydia Miller-Jones' />
-  //   )
-
-  //   const button = renderedComponent.getByText('Save')
-  //   fireEvent.click(button)
-
-  //   expect(renderedComponent.queryByText(/student name cannot be blank/i)).toBeNull();
-  //   expect(onSave).toHaveBeenCalledTimes(1);
-  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  // });
-
-  // it("submits the name entered by the user", () => {
-  //   const onSave = jest.fn();
-  //   const renderedComponent = render(
-  //     <Form interviewers={interviewers} onSave={onSave} />
-  //   );
-
-  //   const input = renderedComponent.getByPlaceholderText("Enter Student Name");
-
-  //   fireEvent.change(input, { target: { value: "Jane Doe" } });
-  //   fireEvent.click(renderedComponent.getByText("Save"));
-
-  //   expect(onSave).toHaveBeenCalledTimes(1);
-  //   expect(onSave).toHaveBeenCalledWith("Jane Doe", null);
-  // });
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
